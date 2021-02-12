80351165c034a786b5f90c98c730cef13d42cfc0
Kijacode <kijadanford@gmail.com>
2021-02-12 17:55:12 +0300

    feat: add iniApp state config

---
+++ lib/app_state/initState.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+
+class InitState with ChangeNotifier {
+  // initialise
+  String _initName;
+
+
+
+
+// selector
+  String get initName => _initName ?? '';
+
+
+
+// reducer
+  void setinitInfo() async{
+
+  }
+
+}
+
+
+
+
+
