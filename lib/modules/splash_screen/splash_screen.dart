283af83d4b8df84fbef5bd2e5ce62a1315afaf0b
Kijacode <kijadanford@gmail.com>
2021-02-12 19:59:26 +0300

    feat: add & intergrate splash screen

--- lib/modules/splash_screen/splash_screen.dart
+++ lib/modules/splash_screen/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:skiza/modules/authentication/auth_home.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,11 +10,42 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 7);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() async {
+    Navigator.push(
+        context, new MaterialPageRoute(builder: (context) => new AuthHome()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("data"),
+    return Scaffold(
+      backgroundColor: Color(0xFFF2F2F2),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 4,
+            ),
+            Center(
+              child: SvgPicture.asset(
+                "assets/images/skeezah_logo.svg",
+                color: Color(0xFF626262),
+                height: 80,
+                width: 80,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
