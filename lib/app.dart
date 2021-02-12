f301b3e5dd56451a00644052000823d188db728c
Kijacode <kijadanford@gmail.com>
2021-02-12 18:03:52 +0300

    feat: file structure, intergrate init app state

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:skiza/app_state/initState.dart';
+import 'package:skiza/modules/splash_screen/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => InitState()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "skiza App",
+          theme: ThemeData(
+              textTheme: textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+              primaryColor:Color(0xFFCAD827) ,
+              visualDensity: VisualDensity.adaptivePlatformDensity),
+          home: SafeArea(child: SplashScreen()),
+        ));
+  }
+}
---
+++ lib/modules/splash_screen/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("data"),
+      ),
+    );
+  }
+}
