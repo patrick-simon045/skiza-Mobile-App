f8c916e99d9a4bd924ec7d1fc0359ca79de05f0a
Kijacode <kijadanford@gmail.com>
2021-02-20 18:31:58 +0300

    feat:add app_bar component

--- lib/core/components/home_app_bar_component.dart
+++ lib/core/components/home_app_bar_component.dart
@@ -1,11 +1,32 @@
-
 import 'package:flutter/material.dart';
+import 'package:skiza/core/components/search_component.dart';
 
 class HomeAppBarComponent extends StatelessWidget {
+  final TextEditingController namecontroller = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: SearchComponentContainerTextField(
+                namecontroller: namecontroller,
+              ),
+            ),
+
+             // Expanded(child: IconButton(icon: (Icon(Icons.book)),onPressed: (){},)),
+            //Expanded(child: IconButton(icon: (Icon(Icons.book)),onPressed: (){},)),
+          
+          ],
+        ),
+        Row(children: [
+          
+        ],)
+      ],
     );
   }
 }
--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:skiza/core/components/home_app_bar_component.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("data"),
+        child:HomeAppBarComponent(),
       ),
     );
   }
