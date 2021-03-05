152a70947425c44b01e0347fb9cd95d28304d659
Kijacode <kijadanford@gmail.com>
2021-03-05 20:12:46 +0300

    feat:add custimize app bar

--- lib/core/components/home_app_bar_component.dart
+++ lib/core/components/home_app_bar_component.dart
@@ -1,31 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:skiza/core/components/search_component.dart';
+import 'package:skiza/core/utils/app_utils.dart';
 
 class HomeAppBarComponent extends StatelessWidget {
   final TextEditingController namecontroller = new TextEditingController();
+
+  void appDrawer(BuildContext context) {
+      Widget default_widget = GestureDetector(
+    child: Text("kija"),
+    onTap: () {
+      Navigator.pop(context,"Hello");
+    },
+  );
+  
+    return AppUtil.showPopUpModal(context, default_widget, true);
+  }
+
+// AppUtil
   @override
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
     double _height = MediaQuery.of(context).size.height;
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Row(
-          children: [
-            Expanded(
-              child: SearchComponentContainerTextField(
-                namecontroller: namecontroller,
-              ),
-            ),
-
-             // Expanded(child: IconButton(icon: (Icon(Icons.book)),onPressed: (){},)),
-            //Expanded(child: IconButton(icon: (Icon(Icons.book)),onPressed: (){},)),
-          
-          ],
-        ),
-        Row(children: [
-          
-        ],)
+        Container(
+            height: _height / 6,
+            width: _width,
+            color: Colors.transparent,
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 7,
+                    child: Container(
+                      color: Colors.transparent,
+                      child: SearchComponentContainerTextField(
+                        namecontroller: namecontroller,
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.transparent,
+                      child: IconButton(
+                          icon: (Icon(
+                            Icons.book,
+                            color: Color(0xFF707070),
+                          )),
+                          onPressed: () {}),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          icon: (Icon(
+                            Icons.directions_railway_outlined,
+                            color: Color(0xFF838383),
+                          )),
+                          onPressed: () => appDrawer(context),
+                        )))
+              ],
+            )),
       ],
     );
   }
--- lib/core/components/search_component.dart
+++ lib/core/components/search_component.dart
@@ -9,27 +9,29 @@ class SearchComponentContainerTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(
-        left: 95, right: 45, top: 10, bottom: 15
+        left: 22, right: 4, top: 1, bottom: 6
         ),
       child: Material(
         elevation: 0,
         child: Container(
-         width:MediaQuery.of(context).size.width/4 ,
+         width:MediaQuery.of(context).size.width/2 ,
           padding: EdgeInsets.symmetric(horizontal: 10),
           decoration: BoxDecoration(
-             color: Colors.red,
+             color: Color(0xFFE4E4E4),
             //color: Color(0xFFE4E4E4),
             borderRadius: BorderRadius.circular(10),
           ),
           child: TextFormField(
             controller: namecontroller,
-            textAlign: TextAlign.start,            
+            textAlign: TextAlign.start, 
+                       
             decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search_outlined),
                 hintStyle: TextStyle(
                     color: Color(0xFFC2C2C2),
                     fontSize: 20,
                     fontWeight: FontWeight.bold),
-                hintText: 'enter searching keyword'),
+                hintText: 'enter word'),
             validator: (value){
              
             },
